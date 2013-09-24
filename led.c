72289ac76e40546723c5c8852c541085409fe5eb
Juan Felipe Martínez <juanfe@Ububtu-PC.(none)>
2013-09-24 12:05:45 -0500

    Subo archivo de prueba led.c

---
+++ led.c
@@ -0,0 +1,48 @@
+#include<avr/io.h>
+#define seg_1 64
+#define seg_2 128
+#define seg_3 196
+#define seg_4 254
+#define sbi(x,y) x |= _BV(y) 
+#define cbi(x,y) x &=~_BV(y)
+
+int main(void)
+{
+    DDRB=0xff; //PORTB as output
+    PORTB=0x00;
+    DDRA=0x00; //PORTA set as input (since we want to take inputs for the ADC)
+    PORTA=0xff; //Enable the pull-ups on PORTA
+
+    ADMUX =_BV(ADLAR); //Enable the Left aligning of the ADC result
+
+    ADCSRA = _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0); //Enable the ADC with prescaler=128
+
+    ADCSRA |=_BV(ADSC); //Set the bit to start the conversion
+    while(1)
+    {
+        while(ADCSRA &_BV(ADSC)) {} //Waits for ADC conversion to complete
+
+        if(ADCH>=seg_4)
+        {
+			sbi(PORTB,PB0); //Suppose a motor between PB0 and PB1 with proper buffers
+			sbi(PORTB,PB1); // In this case, the motor runs forward
+        }
+        else if (ADCH>=seg_3)
+        {
+			sbi(PORTB,PB0); //Here the motor runs backward
+            cbi(PORTB,PB1);
+        }
+		else if (ADCH>=seg_2)
+        {
+			cbi(PORTB,PB0); //Here the motor runs backward
+            sbi(PORTB,PB1);
+        }
+		else if (ADCH>=seg_1)
+        {
+			cbi(PORTB,PB0); //Here the motor runs backward
+            cbi(PORTB,PB1);
+        }
+        ADCSRA |=_BV(ADSC); //Set the bit to start conversion again
+    }
+    return 0;
+}
