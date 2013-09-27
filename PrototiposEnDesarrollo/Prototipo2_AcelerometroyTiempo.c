bd30963f5788a57a47aa60a12c5f551bbce62204
Wallamejorge <wallamejorge@hotmail.com>
2013-09-27 14:32:57 -0500

    Update Prototipo2_AcelerometroyTiempo.c
    Este codigo, mide el angulo en grados y lo imprime en PORD en binario " decodificado de 0 a 1010" y tambien cuenta segundos en PORTC

--- PrototiposEnDesarrollo/Prototipo2_AcelerometroyTiempo.c
+++ PrototiposEnDesarrollo/Prototipo2_AcelerometroyTiempo.c
@@ -21,8 +21,8 @@
 #define sbi(x,y) x |= _BV(y) //Hace set al yth bit del puerto X
 #define cbi(x,y) x &= ~(_BV(y)) //Hace reset al yth bit del puerto X
 #define tbi(x,y) x ^= _BV(y) //Cambia el estado logico del yth bit del puerto X
-#define is_high(x,y) ((x & _BV(y)) == _BV(y)) //Verifica si el yth bit del puerto X es 1
-#define is_low(x,y) ((x & _BV(y)) == 0) //Verifica si el yth bit del puerto X es 0
+#define is_high(x,y) ((x & _BV(y)) == _BV(y))
+#define is_low(x,y) ((x & (_BV(y))) == 0) //check if the input pin is low
 double initial_anglexz(double ax, double ay, double az); // Calcula el angulo xz inicial de lanzamiento
 double initial_anglexy(double ax, double ay, double az); // Calcula el angulo xy inicial de lanzamiento
 double initial_angleyz(double ax, double ay, double az); // Calcula el angulo yz inicial de lanzamiento
@@ -31,6 +31,7 @@ int time_counter(int Reset,int count,int count0); // Contador mientras que este 
 void init_ports(void); //Inicializa los puertos
 void select_ADC_port(int i);// Seleccion cual puerto de ADC leer
 void print_Angle_Binary(int NoPortCD,double angle);// Imprime en PORTD el angulo medido en binario
+void print_Time_Binary(int time);// Imprime en PORTC el tiempo medido en binario
 //------------------------------------------------------//
 
 
@@ -45,20 +46,40 @@ void main(void)
 	double angle0yz=0.0;
 	double angle0xz=0.0;
 	double x_total=0.0;
-	double a[3];
-	int time=0.0;
-	int Count=0;
-	int Reset=0;
+	double a[5];
+	int time=0;
+	double ms_time=0.0;
+	int count=0;
+	int rstanalogo=0;
 
 	while (1)
-	{  for(i=0;i<4;i++){select_ADC_port(i); /*Seleccionar puerto de entrada ADC*/ a[i]=ADCH-81;} // Cargar en el vector a[i] los valores ax ay az
-
+	{  for(i=0;i<6;i++){select_ADC_port(i); /*Seleccionar puerto de entrada ADC*/ a[i]=ADCH-81;} // Cargar en el vector a[i] los valores ax ay az
+	a[3]=a[3]+81;
+	a[4]=a[4]+81;
+	
 		angle0xy=initial_anglexy(a[0],a[1],a[2]); // Genero angulo entre x y y en grados
 		angle0xz=initial_anglexz(a[0],a[1],a[2]); // Genero angulo entre x y z en grados
 		angle0yz=initial_angleyz(a[0],a[1],a[2]); // Genero angulo entre y y z en grados
 		print_Angle_Binary(1,angle0xz); // Imprime el angulo en binario en el puerto PORTD
-		time=time+1;
-		_delay_ms(1);
+		
+		// Conversión de entrada análoga
+		
+		if(a[3]>132){count=1;}
+		else{count=0;}
+		if(a[4]>132){rstanalogo=1;}
+		else{rstanalogo=0;}
+		// Si reset (análogo) todas la señales a 0, sino los contadores de milisegundos y segundos funcionan normal	
+			
+			if (count==1)	{
+				if(ms_time==10){time=time+1;ms_time=0;}else{time=time+0;}
+				_delay_ms(100);
+				ms_time=ms_time+1;	
+				print_Time_Binary(time);}
+			else { ms_time=ms_time;time=time;}
+			
+
+		
+
 		x_total=final_distance(angle0xz,time); // Calcula la distancia final.
 
 		
@@ -79,7 +100,7 @@ void init_ports(void){
 	DDRA=0x00;		//Defino Puerto A como Inputs
 	PORTA=0x00;     //Habilito los puertos de A
 	DDRB = 0x00;    //Defino Puerto B como Inputs
-	PORTB=0xff;     //Habilito los puertos de B
+	PORTB=0x00;     //Habilito los puertos de B
 
 	DDRC=0xff;		//Defino Puerto C como Outputs
 	PORTC=0x00;     //Habilito los puertos de C
@@ -174,6 +195,33 @@ else if(angle>angle_segment_2){cbi(PORTD,PD3);cbi(PORTD,PD2);sbi(PORTD,PD1);sbi(
 else if(angle>angle_segment_1){cbi(PORTD,PD3);cbi(PORTD,PD2);sbi(PORTD,PD1);cbi(PORTD,PD0);} //PORD=0010//
 else if(angle>0){              cbi(PORTD,PD3);cbi(PORTD,PD2);cbi(PORTD,PD1);sbi(PORTD,PD0);} //PORD=0001//
 	
+}
+//------------------------------------------------------//
+
+
+
+//------------------------------------------------------//
+//----------------------Print Angle---------------------//
+//------------------------------------------------------//
+void print_Time_Binary(int time){
+int time_segment_1=1;
+int time_segment_2=2;
+int time_segment_3=3;
+int time_segment_4=4;
+int time_segment_5=5;
+int time_segment_6=6;
+int time_segment_7=7;
+int times=time;
+if(times>time_segment_7){	   sbi(PORTC,PC2);sbi(PORTC,PC1);sbi(PORTC,PC0);} //PORC=111//
+else if(times>time_segment_6){  sbi(PORTC,PC2);sbi(PORTC,PC1);cbi(PORTC,PC0);} //PORD=110//
+else if(times>time_segment_5){  sbi(PORTC,PC2);cbi(PORTC,PC1);sbi(PORTC,PC0);} //PORD=101//
+else if(times>time_segment_4){  sbi(PORTC,PC2);cbi(PORTC,PC1);cbi(PORTC,PC0);} //PORD=100//
+else if(times>time_segment_3){  cbi(PORTC,PC2);sbi(PORTC,PC1);sbi(PORTC,PC0);} //PORD=011//
+else if(times>time_segment_2){  cbi(PORTC,PC2);sbi(PORTC,PC1);cbi(PORTC,PC0);} //PORD=010//
+else if(times>time_segment_1){  cbi(PORTC,PC2);cbi(PORTC,PC1);sbi(PORTC,PC0);} //PORD=001//
+else {			               cbi(PORTC,PC2);cbi(PORTC,PC1);cbi(PORTC,PC0);} //PORD=000//
+
+
 }
 //------------------------------------------------------//
 
